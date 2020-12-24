ca031999d3199871e0528a9066e8ebf17028bcfb
satu-n <49983831+satu-n@users.noreply.github.com>
2020-12-24 10:39:12 +0900

    api: formatted main.rs

--- api/src/main.rs
+++ api/src/main.rs
@@ -16,78 +16,77 @@ mod utils;
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     dotenv::dotenv().ok();
-    std::env::set_var("RUST_LOG",
-        format!("{}=DEBUG,actix_web=INFO,actix_server=INFO",
-            utils::env_var("APP_NAME")),
-    );
+    std::env::set_var("RUST_LOG", format!(
+        "{}=DEBUG,actix_web=INFO,actix_server=INFO",
+        utils::env_var("APP_NAME")
+    ));
     env_logger::init();
-    
-    // create db connection pool
-    let db_url = utils::env_var("DATABASE_URL");
-    let manager = ConnectionManager::<PgConnection>::new(db_url);
+
     let pool: models::Pool = r2d2::Pool::builder()
-        .build(manager)
-        .expect("Failed to create pool.");
+        .build(ConnectionManager::<PgConnection>::new(
+            utils::env_var("DATABASE_URL")
+        )).expect("Failed to create pool.");
 
     HttpServer::new(move || {
-        let cors = Cors::permissive(); // TODO tighten for production
         App::new()
-            .wrap(cors)
-            .data(pool.clone())
-            .wrap(middleware::Logger::default())
-            .wrap(IdentityService::new(
-                CookieIdentityPolicy::new(utils::SECRET_KEY.as_bytes())
-                    .name("auth")
-                    .path("/")
-                    // .domain(utils::env_var("COOKIE_DOMAIN").as_str()) // TODO if cross domain
-                    .max_age(86400)
-                    .secure(
-                        utils::env_var("API_PROTOCOL") == "https"
-                        ), // TODO https
-            ))
-            .data(web::JsonConfig::default().limit(4096))
-            .service(web::scope("/api")
-
-                .service(web::resource("/invite")
-                    .route(web::post().to(handlers::invite::invite)),
-                )
-                .service(web::resource("/register")
-                    .route(web::post().to(handlers::register::register)),
-                )
-                .service(web::resource("/auth")
-                    .route(web::get().to(handlers::auth::get_me))
-                    .route(web::post().to(handlers::auth::login))
-                    .route(web::delete().to(handlers::auth::logout)),
-                )
-                .service(web::scope("/app")
-                    .wrap_fn(|req, srv| {
-                        use actix_identity::RequestIdentity;
-                        if req.get_identity().is_some() {
-                            use actix_service::Service;
-                            srv.call(req)
-                        } else {
-                            use actix_web::{HttpResponse, dev};
-                            use futures::future::{Either, ok};
-                            Either::Right(ok(dev::ServiceResponse::new(
-                                req.into_parts().0,
-                                HttpResponse::Unauthorized().finish()
-                            )))
-                        }
-                    })
-                    .service(web::resource("/tasks")
-                        .route(web::get().to(handlers::app::home::home))
-                        // .route(web::post().to(handlers::app::text::text))
-                        // .route(web::put().to(handlers::app::clone::clone))
-                        // .route(web::delete().to(handlers::app::exec::exec)),
-                    )
-                    .service(web::resource("/task/{tid}")
-                        // .route(web::get().to(handlers::app::focus::focus))
-                        // .route(web::put().to(handlers::app::star::star)),
-                    ),
-                ),
+        .data(pool.clone())
+        .wrap(middleware::Logger::default())
+        .wrap(Cors::permissive()) // TODO tighten for production
+        .wrap(IdentityService::new(
+            CookieIdentityPolicy::new(utils::SECRET_KEY.as_bytes())
+            .name("auth")
+            .path("/")
+            // .domain(utils::env_var("COOKIE_DOMAIN").as_str()) // TODO if cross domain
+            .max_age(86400)
+            .secure(utils::env_var("API_PROTOCOL") == "https") // TODO https
+        ))
+        .data(web::JsonConfig::default().limit(4096))
+        .service(web::scope("/api")
+            .service(web::resource("/invite")
+                .route(web::post().to(handlers::invite::invite))
             )
+            .service(web::resource("/register")
+                .route(web::post().to(handlers::register::register))
+            )
+            .service(web::resource("/auth")
+                .route(web::get().to(handlers::auth::get_me))
+                .route(web::post().to(handlers::auth::login))
+                .route(web::delete().to(handlers::auth::logout))
+            )
+            .service(web::scope("/app")
+                .wrap_fn(|req, srv| {
+                    use actix_identity::RequestIdentity;
+                    if req.get_identity().is_some() {
+                        use actix_service::Service;
+                        srv.call(req)
+                    } else {
+                        use actix_web::{dev, HttpResponse};
+                        use futures::future::{ok, Either};
+                        Either::Right(ok(dev::ServiceResponse::new(
+                            req.into_parts().0,
+                            HttpResponse::Unauthorized().finish()
+                        )))
+                    }
+                })
+                .configure(auth_protected)
+            )
+        )
     })
-    .bind(format!("0.0.0.0:3000"))?
+    .bind(utils::env_var("SOCKET_ADDRESS"))?
     .run()
     .await
+}
+
+fn auth_protected(cfg: &mut web::ServiceConfig) {
+    cfg
+    .service(web::resource("/tasks")
+        .route(web::get().to(handlers::app::home::home))
+        // .route(web::post().to(handlers::app::text::text))
+        // .route(web::put().to(handlers::app::clone::clone))
+        // .route(web::delete().to(handlers::app::exec::exec))
+    )
+    .service(web::resource("/task/{tid}")
+        // .route(web::get().to(handlers::app::focus::focus))
+        // .route(web::put().to(handlers::app::star::star))
+    );
 }
